bc9840ad347591b67c36680909be4a3bf2f32e49
NaxtorGames <72697963+NaxtorGames@users.noreply.github.com>
2025-05-16 14:06:52 +0200

    Project rebuild.

--- fileSaver.js
+++
@@ -1,9 +0,0 @@
-﻿function downloadFile(filename, contentType, content) {
-    const blob = new Blob([content], { type: contentType });
-    const link = document.createElement("a");
-    link.href = URL.createObjectURL(blob);
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-}
