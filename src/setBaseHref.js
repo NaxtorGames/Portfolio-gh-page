dce8bae4c025c8b4d0f8f7583472ea13e04334dc
NaxtorGames <72697963+NaxtorGames@users.noreply.github.com>
2025-05-16 13:38:20 +0200

    Godot game implemented. (Testing on website)

---
+++ src/setBaseHref.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    let baseHref = "/";
+    if (window.location.host.includes("github.io")) {
+        baseHref = "/Portfolio-gh-page/"; // Dein Repo-Name hier
+    }
+    document.getElementById("dynamic-base").setAttribute("href", baseHref);
+})();
